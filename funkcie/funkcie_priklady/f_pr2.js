79a4ab2117c55f0bd807ba7eab47de88f52afa4b
Matej Krajcovic <matej.krajcovic3@gmail.com>
2024-02-10 21:23:27 +0100

    d

--- funkcie/funkcie_priklady/f_pr2.js
+++
@@ -1,9 +0,0 @@
-
-
-
-
-const showImageButton = document.getElementsByClassName("show-image-button");
-const myImage = document.getElementsByClassName("my-image");
-showImageButton.addEventListener("click", () => { 
-   myImage.classList.toggle("visible"); 
-});
